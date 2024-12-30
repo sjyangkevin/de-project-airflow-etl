81cb5f4ac3d02edc8763168ef8b080b0c58a060a
Kevin Yang <kevinsjy997@gmail.com>
2024-12-30 12:37:58 -0500

    modify setup script

--- setup-script.sh
+++ setup-script.sh
@@ -2,7 +2,4 @@ mkdir -p ./logs ./plugins ./config
 if [[ -f .env ]]; then
     rm .env
 fi
-if [[ -z "${AIRFLOW_UID}" ]]; then
-    echo -e "AIRFLOW_UID=$(id -u)" >> .env
-fi
 echo -e "AIRFLOW_PROJ_DIR=$(pwd)" >> .env
